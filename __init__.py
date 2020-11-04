bea9c2302e920a8dbc9961bcd70dd26d98dd1197
murx- <till@fam-os.org>
2020-11-04 16:32:17 +0100

    Add rudimentary multimodule support

--- __init__.py
+++ __init__.py
@@ -4,10 +4,13 @@ from binaryninja import InstructionTextTokenType
 from binaryninja import demangle
 from binaryninja import log
 import json
+import traceback
+import os
 
 class binja_devi():
 
     def __init__(self, bv):
+        self.version = 0.2
         self.bv = bv
         self.call_cnt = 0
         self.load_virtual_calls()
@@ -16,10 +19,48 @@ class binja_devi():
     def load_virtual_calls(self):
         json_file = interaction.get_open_filename_input("Load virtual calls", "*.json")
         with open(json_file.decode("utf-8")) as f:
-            json_objects = json.load(f)
-        self.devirtualize_calls(json_objects["calls"])
+            devi_json_data = json.load(f)
+                
+        if self.version < devi_json_data["deviVersion"]:
+            print("[!] devi JSON file has a more recent version than IDA plugin!")
+            print("[!] we try parsing anyway!")
+        if self.version > devi_json_data["deviVersion"]:
+            print("[!] Your devi_ida and devi_frida versions are out of sync. Update your devi_ida!")
 
-    def devirtualize_calls(self, call_list):
+        if self.version == devi_json_data["deviVersion"]:
+            self.devirtualize_calls(devi_json_data["calls"], devi_json_data["modules"])
+        elif devi_json_data["deviVersion"] == 0.1:
+            self.devirtualize_calls_v01(devi_json_data["calls"])
+
+
+    def devirtualize_calls(self, call_list, modules):
+        binja_filename = os.path.basename(self.bv.file.filename)
+
+        for module in modules:
+            if module["name"] == binja_filename:
+                loaded_module = module
+                break
+        
+        start = int(loaded_module["base"], 16)
+        end = start + loaded_module["size"]
+
+        print("[!] Adding virtual calls for {binja_filename}")
+
+        for v_call in call_list:
+            for call in v_call:
+                if start <= int(call, 16) <= end:
+
+                    src = int(call, 16) - start
+                    dst = int(v_call[call]) - start
+
+                    self.caller = self.bv.get_functions_containing(src)[0]
+                    self.caller.add_user_code_ref(src, dst, from_arch=None)
+                    self.add_call_comment(src, dst)
+                    self.call_cnt += 1
+        log.log(1, "Devirtualized {} calls".format(self.call_cnt))
+
+
+    def devirtualize_calls_v01(self, call_list):
         for v_call in call_list:
             for call in v_call:
                 to_addr = int(v_call[call])
